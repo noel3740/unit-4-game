e5484a711116e77a0f9767500f2dd4189583d374
Noel Nevarez <noel3740@gmail.com>
2018-10-20 16:46:41 -0500

    Cleaning up code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,37 @@
 //Define global variables
-var yourCharacterDiv;
-var defenderCharacterDiv;
 var resultsDiv = $(".result");
 var attackButton = $("#attackButton");
 var battleSectionDiv = $(".battleSection");
+var enemySelectionSectionDiv = $(".enemySelectionSection");
 var selectCharacterSectionDiv = $(".selectCharacter");
 var winAudio = document.getElementById("winAudio");
 
+var yourCharacter = {
+    health: 0,
+    attackPower: 0,
+    name: "",
+    cardDiv: undefined
+}
+
+var defender = {
+    health: 0,
+    counterAttackPower: 0,
+    name: "",
+    cardDiv: undefined
+}
+
 //Function will run when character is chosen
-function characterChosen (characterDiv) {
+function yourCharacterChosen (characterDiv) {
 
     //Show the select battle section and hide the character select section
     selectCharacterSectionDiv.hide();
     battleSectionDiv.show();
 
-    //Set the selected character global variable to the current div
-    yourCharacterDiv = characterDiv;
+    //Set the selected character global object
+    yourCharacter.cardDiv = characterDiv;
+    yourCharacter.name = characterDiv.find(".characterName").text();
+    yourCharacter.attackPower =  parseInt(characterDiv.attr("attackPower"));
+    yourCharacter.health = parseInt(characterDiv.attr("healthPoints"));
 
     //Add isYourCharacter attribute to the characterDiv*/
     characterDiv.attr("isYourCharacter", "1");
@@ -35,40 +51,51 @@ function characterChosen (characterDiv) {
     });
 }
 
-//Function will add a defender to the fight section
-function addDefender (enemyCharacterDiv) {
+//Function will be run when a defender is chosen
+function defenderChosen (characterDiv) {
 
-    defenderCharacterDiv = enemyCharacterDiv;
+    //Set the selected character global object
+    defender.cardDiv = characterDiv;
+    defender.name = characterDiv.find(".characterName").text();
+    defender.counterAttackPower =  parseInt(characterDiv.attr("counterAttackPower"));
+    defender.health = parseInt(characterDiv.attr("healthPoints"));
 
     //Clear the results div.
     resultsDiv.text("");
 
     //Remove enemy character from the available to be selected section and move to the defender/enemy section
-    enemyCharacterDiv.detach();
-    $(".defender").append(enemyCharacterDiv);
+    characterDiv.detach();
+    $(".defender").append(characterDiv);
 
     //Remove clickable class from all enemy divs 
     $(".character[isYourCharacter!='1']").removeClass("clickable");
 
     //Enable attack button 
     attackButton.prop('disabled', false);
+
+    //If there is only one remaining enemy (the one that was chosen) then hide the enemy selection section 
+    if ($(".enemies .character").length <= 0) {
+        enemySelectionSectionDiv.hide();
+    }
 }
 
 //Function will restart the game
 function restart () {
     //Reset global variables
-    yourCharacterDiv = undefined;
-    defenderCharacterDiv = undefined;
+    yourCharacter.cardDiv = undefined;
+    defender.cardDiv = undefined;
 
-    //Show the select character section and hide the battle section
-    selectCharacterSectionDiv.show();
+    //Hide the battle section, show the select character section, 
+    //and show the enemy selection section (will not be visible because it's parent is the battle section which was hidden)
     battleSectionDiv.hide();
+    selectCharacterSectionDiv.show();
+    enemySelectionSectionDiv.show();
 
     //Disable the attack button 
     attackButton.prop('disabled', true);
 
-    //Reset the character health and attack
-    resetCharacterCurrentHealthAndAttack();
+    //Reset the character health on screen
+    resetAllCharactersHealthOnScreen();
 
     //Clear the result div
     resultsDiv.text("");
@@ -92,85 +119,67 @@ function restart () {
     });
 }
 
-//Function to reset all the characters health and attack
-function resetCharacterCurrentHealthAndAttack() {
+//Function to reset all the characters health on screen
+function resetAllCharactersHealthOnScreen() {
     
     //Loop through all the characters
     $(".character").each(function() {
-        var characterDiv = $(this);
-
-        //Get the healthPoints attribute value
-        var healthPoints = characterDiv.attr("healthPoints");
-
-        //Set the characters current health points
-        setCurrentHealthPoints(characterDiv, healthPoints);        
-
-        //Reset current attack power to attack power attribute
-        characterDiv.attr("currentAttackPower", characterDiv.attr("attackPower"));
+        //Set the characters current health points to their respective health points element attribute
+        setHealthPointsOnScreen($(this), $(this).attr("healthPoints"));        
     });
 }
 
 //Function to be run when the user attacks an enemy
 function attack() {
 
-    //Store in variables the current health points, current attack power and attack power attributes for user's character and convert those values to integer
-    var yourHealth = parseInt(yourCharacterDiv.attr("currentHealthPoints"));
-    var yourAttackPower = parseInt(yourCharacterDiv.attr("currentAttackPower"));
-    var yourOriginalAttackPower = parseInt(yourCharacterDiv.attr("attackPower"));
-
-    //Store in variables the current health points and counter attack power attributes from the enemy/defender character and convert those values to integer
-    var defenderHealth = parseInt(defenderCharacterDiv.attr("currentHealthPoints"));
-    var defenderCounterAttackPower = parseInt(defenderCharacterDiv.attr("counterAttackPower"));
-
-    //Get the enemy/defender's name and place it in a variable
-    var defenderName = defenderCharacterDiv.find(".characterName").text();
-
-    //Display to the user how many health points they attacked for and how much the enemy/defender counter attacked their character for. 
-    resultsDiv.text("You attached " + defenderName + " for " + yourAttackPower + " damage. " + defenderName + " attacked you back for " + defenderCounterAttackPower + " damage.");
+    //Display to the user how many health points they attacked for 
+    //and how much the enemy/defender counter attacked their character for. 
+    resultsDiv.text("You attacked " + defender.name + " for " + yourCharacter.attackPower + " damage. " + defender.name + " attacked you back for " + defender.counterAttackPower + " damage.");
 
     //Subtract from the defender/enemy health the amount of current attack power the user's character has
-    defenderHealth -= yourAttackPower;
+    defender.health -= yourCharacter.attackPower;
 
-    //Increase user's attack power by the amount of the user's character original attack power and set the character div attribute value to the value
-    yourAttackPower += yourOriginalAttackPower;
-    yourCharacterDiv.attr("currentAttackPower", yourAttackPower);
+    //Increase user's attack power by the amount of the user's character original attack power 
+    //and set the character div attribute value to the value
+    yourCharacter.attackPower += parseInt(yourCharacter.cardDiv.attr("attackPower"));
 
-    //If the user's character health is greater than 0 and the defender's health is 0 or less then the user defeated the enemy/defender
-    if (yourHealth > 0 && defenderHealth <= 0) {
-        playerDefeatedEnemy(defenderName);
+    //If the user's character health is greater than 0 and the defender's health is 0 
+    //or less then the user defeated the enemy/defender
+    if (yourCharacter.health > 0 && defender.health <= 0) {
+        playerDefeatedEnemy();
     }    
     //If the user's charcter did not win then allow the enemy to counter attack
     else {
         //Subtract from the user's health the amount of counter attack power the defender/enemy has
-        yourHealth -= defenderCounterAttackPower;
+        yourCharacter.health -= defender.counterAttackPower;
 
         //If user's character health is less than or equal 0 and the defender's health 
         //is greater than or equal 0 then the user lost
-        if (yourHealth <= 0 && defenderHealth >= 0) {
+        if (yourCharacter.health <= 0 && defender.health >= 0) {
             playerLost();
         }
     }
 
     //Set the user's or defender's character health to 0 if either is less than 0
-    if (yourHealth < 0 ) { yourHealth = 0; }
-    if (defenderHealth < 0) { defenderHealth = 0; }
+    if (yourCharacter.health < 0 ) { yourCharacter.health = 0; }
+    if (defender.health < 0) { defender.health = 0; }
 
-    //Set the current health points attributes for both user's character and defender's character and display on character card
-    setCurrentHealthPoints(yourCharacterDiv, yourHealth);
-    setCurrentHealthPoints(defenderCharacterDiv, defenderHealth);
+    //Display current health points on character card
+    setHealthPointsOnScreen(yourCharacter.cardDiv, yourCharacter.health);
+    setHealthPointsOnScreen(defender.cardDiv, defender.health);
 }
 
 //Function to be run when the enemy has been defeated
-function playerDefeatedEnemy(defenderName) {
+function playerDefeatedEnemy() {
 
-    //Assign the defeated flag
-    defenderCharacterDiv.attr("isDefeated", "1");
+    //Assign the defeated flag element attribute
+    defender.cardDiv.attr("isDefeated", "1");
     
     //Disable the attack button
     attackButton.prop('disabled', true);
 
-    //Get all enemies that are undefeated (all character class divs where isYourCharater attribute is not 1 and isDefeated attribute is not 1)
-    var undefeatedEnemies = $(".character[isYourCharacter !='1'][isDefeated != '1']");
+    //Get all enemies that are undefeated (all characters left in the "select enemy to attack section")
+    var undefeatedEnemies = $(".enemies .character");
 
     //If there are no more undefeated enemies then the player won
     if (undefeatedEnemies.length <= 0) {
@@ -182,7 +191,7 @@ function playerDefeatedEnemy(defenderName) {
     //There are still enemies to be defeated
     else {
         //Display to the user that they defeated the enemy and that they can choose another enemy to fight
-        resultsDiv.text("You have defeated " + defenderName + ". You can choose to fight another enemy.");
+        resultsDiv.text("You have defeated " + defender.name + ". You can choose to fight another enemy.");
         //Add the clickable class back to all the undefeated enemies
         undefeatedEnemies.addClass("clickable");
     }
@@ -198,38 +207,36 @@ function playerLost() {
     attackButton.prop('disabled', true);
 }
 
-//Set current helth points function
-function setCurrentHealthPoints(characterDiv, healthPoints) {
+//Set current health points on screen function
+function setHealthPointsOnScreen(characterDiv, healthPoints) {
     /*Set the character health span text to the health points*/
     characterDiv.find(".characterHealth span").text(healthPoints);
-    /*Set current health points attribute*/
-    characterDiv.attr("currentHealthPoints", healthPoints);
 }
 
-//Attach on click even to restart button
+//Will be run when a character card is selected
+function characterClicked(characterDiv) {
+    //Your character has not been chosen
+    if (!yourCharacter.cardDiv) {
+        yourCharacterChosen(characterDiv);
+    }
+    //Character has already been chosen and the character card is clickable (aka has the clickable class)
+    else if (characterDiv.hasClass("clickable")) {
+        //Run the defender has been chosen function
+        defenderChosen(characterDiv);
+    }
+}
+
+//Attach on click event to restart button
 $("#restartButton").on("click", restart);
 
-//Attach on click even to attack button
+//Attach on click event to attack button
 attackButton.on("click", attack);
 
-//Attach on click even to character divs
-$(".character").on("click", function(event){
-    var characterDiv = $(this);
+//Attach on click event to character card divs
+$(".character").on("click", function() { characterClicked($(this)); });
 
-    //Your character has not been chosen
-    if (!yourCharacterDiv) {
-        characterChosen(characterDiv);
-    }
-    //Character has already been chosen
-    else if (characterDiv &&
-        characterDiv.hasClass("clickable")) {
-        //Add the defender to the fight section
-        addDefender(characterDiv);
-    }
-});
-
-//Call the reset character health and attack function
-resetCharacterCurrentHealthAndAttack();
+//Reset the health of all the characters on screen
+resetAllCharactersHealthOnScreen();
 
 //Hide the battle section initially
 battleSectionDiv.hide();
